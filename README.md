fddac1da7beaa6ccc3c4330f2f6da7de5c6182f5
NovaViper <nova.code17@gmail.com>
2021-11-30 00:17:49 -0600

    Added graphic env, grub, more env vars, audio selector, lots more

--- README.md
+++ README.md
@@ -4,29 +4,29 @@ This script helps me personally install ArchLinux along with most of the package
 
 ## What it does
 - Automatically configures ArchLinux in chroot environment, as per the [Installation Guide](https://wiki.archlinux.org/title/installation_guide)
-- Installs various packages for basic system with a DE included (currently supports XFCE, KDE, Gnome, and Cinnamon).
+- Installs various packages for basic terminal use (i.e. neovim) and has optional DE install optional.
 - Adds parameters needed for LUKS, BTRFS, LVM and Swapfile
 - Sets up XDG folder specifications for users and environment variables
-- Installs and Configurations ZSH and Neofetch with basic and advanced configurations (just change the remove the .pending tag off the file names)
+- Installs and Configurations ZSH, Neovim and Neofetch with basic and advanced configurations (just change the remove the .pending tag off the file names for the advanced configurations)
 - Can install aditional packages for laptops, like TLP and Wacom touchscreen support
 - Installs Libvirt and Virt manager, as well as configures all users for libvirt
 - Configures sudoers (adds all user accounts to wheel group)
 - Saves script output in a log.txt file
 - Installs yay AUR helper
+- Installs and Configures Systemd-boot and Grub
 
 ## What it doesn't do
 - Automatically add kernel parameters for LUKS, BTRFS LVM, and SWAP for mkinicpio, it just echoes the names of the paramters into the mkinitcpio file
 - Locate the UUID for the storage device itself, it can get the UUID for the partitions however (this is crucial particuaraly for LUKS as it **requires** the UUID of the *storage device*, not the parition). You must manually locate the storage device's UUID via the `blkid -o list` command. See [Configuring Bootloader for LUKS](https://wiki.archlinux.org/title/Dm-crypt/Encrypting_an_entire_system#Configuring_the_boot_loader).
 - Install AUR packages, those must be installed after the system is booted and logged into the *user* account.
-- Setup ZSH and Neovim WITH plugins, it does provide a basic config for both programs, but you must install Zplug and NVimPlug (which are only available as AURs)
+- Setup ZSH and Neovim WITH plugins, it does provide a basic config for both programs, but you must install Zplug and NVimPlug (which are only available as AURs). After you install Zplug and NVimPlug, you may remove the .pending tag off the end of the file name (which will overwrite the minimal configurations).
 - Configure keyboard layout, other langauges, and other timezones. The script must be modified in order to account for those
-- Install Grub, it only installs Systemd-boot as that was the one that was easiest for me to install on my computer.
 - Check if the script was ran once already, **This is EXTREMELY important as the script will rerun all of the commands again, which will break things!**
-- Support anything that isn't swapfiles.
+- Support anything  that isn't swapfiles.
 - Configure partitions, you should have already done this prior to getting into chroot!
 - Enable Emacs client systemd user script, since Emacs systemd requires you to run it on a user account, the script obviously cannot enable it in this phase, you will have to enable it after you reboot.
-- Configure DE theme. The theme I use, (Dracula)[https://draculatheme.com/] is only available on the AUR, which cannot be installed from chroot.
-- Configure other packages and desktop environment settings. Also is complicated to setup while in chroot, but mainly unsure what folders need to be changed. Might get around to add this feature if it's possible 
+- Configure DE theme. The theme I use, [Dracula](https://draculatheme.com/) is only available on the AUR, which cannot be installed from chroot.
+- Configure other packages and desktop environment settings. This is also complicated to setup while in chroot, but mainly unsure what folders need to be changed. Might get around to add this feature if it's possible.
 
 ## How to run this script
 - Simply clone the entire repo onto your computer using `git clone https://gitlab.com/NovaViper/archlinux-install-script/`
--- basic-efi.sh
+++ basic-efi.sh
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env bash
 
 #Enable logging!
@@ -6,12 +7,15 @@ exec &> >(tee ./logs.txt)
 
 # Variable declarations
 users=()
+use_graphics=""
 use_lvm=""
 use_btrfs=""
 use_crypt=""
 use_bluetooth=""
 use_swap=""
 microcode=""
+is_laptop=""
+
 
 #Functions
 function ask_yes_or_no() {
@@ -68,7 +72,6 @@ addUserPass(){
 
 
 
-
 ################ START OF SCRIPT ##############
 echo "Enabling Multilib library and configuring Pacman"
 sed -i '33s/.//' /etc/pacman.conf
@@ -93,8 +96,8 @@ echo "::1       localhost" >> /etc/hosts
 addRootPass
 
 echo "========= Installing Base packages and wireless modules ========="
-pacman -S --needed --noconfirm base base-devel linux linux-firmware git gnupg zsh networkmanager network-manager-applet networkmanager-openvpn dialog wpa_supplicant wireless_tools netctl inetutils openssh openvpn openssh-askpass
-pacman -U yay-bin-11.0.2-1-x86_64.pkg.tar.zst
+pacman -S --needed --noconfirm base base-devel linux linux-firmware git gnupg zsh networkmanager dialog wpa_supplicant wireless_tools netctl inetutils openssh openvpn
+pacman -U --needed --noconfirm yay-bin-11.0.2-1-x86_64.pkg.tar.zst
 systemctl enable NetworkManager
 systemctl enable sshd
 
@@ -104,8 +107,7 @@ pacman -S --needed --noconfirm ntfs-3g nfs-utils e2fsprogs smartmontools btrfs-p
 
 
 echo "========= Installing Extra packages ========= "
-pacman -S --needed git neovim emacs yadm neofetch
-
+pacman -S --needed git neovim yadm neofetch
 
 echo "========= Installing User directories and updating ========="
 pacman -S --needed --noconfirm xdg-user-dirs xdg-utils
@@ -116,17 +118,24 @@ echo 'export XDG_CONFIG_HOME="$HOME/.config"' >> /etc/profile
 echo 'export XDG_CACHE_HOME="$HOME/.cache"' >> /etc/profile
 echo 'export XDG_DATA_HOME="$HOME/.local/share"' >> /etc/profile
 echo 'export XDG_STATE_HOME="$HOME/.local/state"' >> /etc/profile
-echo 'export ICEAUTHORITY="${XDG_CACHE_HOME}/ICEauthority"' >> /etc/profile
-echo 'export LESSHISTFILE="${XDG_CONFIG_HOME}/less/history"' >> /etc/profile
-echo 'export LESSKEY="${XDG_CONFIG_HOME}/less/keys"' >> /etc/profile
-echo 'export XAUTHORITY="$XDG_CACHE_HOME/Xauthority"' >> /etc/profile
+
+echo 'export GOPATH="$XDG_DATA_HOME/go"' >> /etc/profile
+echo 'export CARGO_HOME="$XDG_DATA_HOME/cargo"' >> /etc/profile
+echo 'export LESSHISTFILE="$XDG_CONFIG_HOME/less/history"' >> /etc/profile
+echo 'export LESSKEY="$XDG_CONFIG_HOME/less/keys"' >> /etc/profile
+echo 'export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm"' >> /etc/profile
 
 echo "QT_STYLE_OVERRIDE=kvantum" >> /etc/environment
 echo "GTK_THEME='Ant-Dracula'" >> /etc/environment
 
 echo "========= Configuring ZSH ========="
+# Change root shell to ZSH
+chsh -s /bin/zsh
+
 #Place zsh files in /etc/zsh folder for system-wide use
 cp ./zshenv /etc/zsh/zshenv
+cp ./zshrc /etc/zsh/.zshrc.pending
+cp ./zshrc_min /etc/zsh/.zshrc
 
 #Place zshrc files in skel folder so users can get them
 mkdir /etc/skel/.config
@@ -163,63 +172,22 @@ if [[ "${users[@]}" ]]; then
     done
 fi
 
-
-echo "========= Installing ALSA and Audio protocols ========="
-pacman -S --needed --noconfirm alsa-utils pulseaudio gst-libav gst-plugins-ugly gst-plugins-bad pulseaudio-alsa pipewire-alsa
-
-
-if [[ "yes" == $(ask_yes_or_no "Would you like to download and enable Bluetooth?") ]]; then
-    echo "========= Installing Bluetooth ========="
-    use_bluetooth="yes"
-    pacman -S --needed --noconfirm bluez bluez-utils pulseaudio-bluetooth
-    systemctl enable bluetooth
-fi
-
-
-if [[ "yes" == $(ask_yes_or_no "Would you like to install HP Printer Modules?") ]]; then
-    echo "========= Installing HP modules ========="
-    pacman -S --needed --noconfirm cups cups-filters hplip
-    systemctl enable cups
-fi
-
-
-#Installer based on instructions from: https://boseji.com/posts/manjaro-kvm-virtmanager/
-if [[ "yes" == $(ask_yes_or_no "Would you like to install Virt-Manager?") ]]; then
-    echo "========= Installing Virt-Manager, Qemu and other required packages ========="
-    echo "Note: The package iptables-nft will conflict with iptables, please allow iptables-nft to install in order to use Virt-Manager's virutal ethernet feature."
-    sleep 5
-    pacman -S --needed qemu libvirt iptables-nft dnsmasq virt-manager virt-viewer bridge-utils dmidecode edk2-ovmf
-    systemctl enable libvirtd
-    echo "====== Configuring KVM ======"
-    sed -i '/unix_sock_group/s/^#//g' /etc/libvirt/libvirtd.conf
-    sed -i '/unix_sock_rw_perms/s/^#//g' /etc/libvirt/libvirtd.conf
-    virsh net-autostart default
-
-    if [[ "${users[@]}" ]]; then
-        echo "====== Configuring additional users for libvirt ======"
-        echo
-        for i in "${users[@]}"; do
-            echo "Adding $i to libvirt group"
-            usermod -a -G libvirt $i
-        done
-    fi
+if [[ "yes" == $(ask_yes_or_no "Do you want to install a graphical environment?") ]]; then
+    echo "Ok, I will take you to the graphics installer, but first we still have some things to configure."
+    use_graphics="yes"
+    sleep 2
 fi
 
 
 if [[ "yes" == $(ask_yes_or_no "Are you installing on a laptop?") ]]; then
     echo "========= Installing TLP and other battery management tools ========="
+    is_laptop="yes"
     pacman -S --noconfirm --needed acpi acpi_call tlp
     systemctl enable tlp
-
-    if [[ "yes" == $(ask_yes_or_no "Does your laptop have touchscreen capability?")  ]]; then
-        echo "========= Installing Wacom settings ========="
-        pacman -S --noconfirm --needed libwacom xf86-input-wacom
-    fi
 fi
 
 
 if [[ "yes" == $(ask_yes_or_no "Did you use BTRFS for the file system?") ]]; then use_btrfs="yes"; fi
-
 
 if [[ "yes" == $(ask_yes_or_no "Did you use LVM for the file system?") ]]; then
     echo "========= Installing LVM related packages ========="
@@ -227,9 +195,7 @@ if [[ "yes" == $(ask_yes_or_no "Did you use LVM for the file system?") ]]; then
     pacman -S --noconfirm --needed lvm2
 fi
 
-
 if [[ "yes" == $(ask_yes_or_no "Did you use LUKS disk encryption?") ]]; then use_crypt="yes"; fi
-
 
 if [[ "yes" == $(ask_yes_or_no "Do you want to use SWAP on your system?") ]]; then use_swap="yes"; fi
 
@@ -252,25 +218,93 @@ while true; do
 done
 
 
-#Graphics Installer
+## Graphics installer
+if [[ "$use_graphics" = "yes" ]]; then
+    echo "Ok, so here we will go ahead and setup everything for the graphical environment!"
+    sleep 2
+
+    #Network Manager
+    echo "======= Installing GUI components for Network Manager ========"
+    pacman -S --needed --noconfirm network-manager-applet networkmanager-openvpn openvpn
+
+    #Bluetooth
+    if [[ "yes" == $(ask_yes_or_no "Would you like to download and enable Bluetooth?") ]]; then
+        echo "========= Installing Bluetooth ========="
+        use_bluetooth="yes"
+        pacman -S --needed --noconfirm bluez bluez-utils
+        systemctl enable bluetooth
+    fi
+
+    #Laptop Touchscreen
+    if [[ "$is_laptop" = "yes" && "yes" == $(ask_yes_or_no "Does your laptop have touchscreen capability?") ]]; then
+        echo "========= Installing Wacom settings ========="
+        pacman -S --noconfirm --needed libwacom xf86-input-wacom
+    fi
+
+    #Audio Selection
+    while true; do
+        read -p "What audio driver would you like to install? Pulse[A]udio or Pipe[W]ire?: " audio
+        case $audio in
+            A | a)
+                echo "========= Installing PulseAudio protocols ========="
+                pacman -S --needed --noconfirm alsa-utils pulseaudio pulseaudio-alsa pipewire-alsa gst-libav gst-plugins-ugly gst-plugins-bad
+                break;;
+            W | w)
+                echo "========= Installing PipeWire protocols ========="
+                pacman -S --needed --noconfirm alsa-utils pipewire pipewire-media-session pipewire-pulse pipewire-alsa gst-libav gst-plugins-ugly gst-plugins-bad
+                break;;
+            *) echo "Invaild Input";;
+        esac
+    done
+
+    #HP Printer configuration
+    if [[ "yes" == $(ask_yes_or_no "Would you like to install HP Printer Modules?") ]]; then
+        echo "========= Installing HP modules ========="
+        pacman -S --needed --noconfirm cups cups-filters hplip
+        systemctl enable cups
+    fi
+
+    #Installer based on instructions from: https://boseji.com/posts/manjaro-kvm-virtmanager/
+    if [[ "yes" == $(ask_yes_or_no "Would you like to install Virt-Manager?") ]]; then
+        echo "========= Installing Virt-Manager, Qemu and other required packages ========="
+        echo "Note: The package iptables-nft will conflict with iptables, please allow iptables-nft to install in order to use Virt-Manager's virutal ethernet feature."
+        sleep 5
+        pacman -S --needed qemu libvirt iptables-nft dnsmasq virt-manager virt-viewer bridge-utils dmidecode edk2-ovmf
+        systemctl enable libvirtd
+        echo "====== Configuring KVM ======"
+        sed -i '/unix_sock_group/s/^#//g' /etc/libvirt/libvirtd.conf
+        sed -i '/unix_sock_rw_perms/s/^#//g' /etc/libvirt/libvirtd.conf
+        virsh net-autostart default
+
+        if [[ "${users[@]}" ]]; then
+            echo "====== Configuring additional users for libvirt ======"
+            echo
+            for i in "${users[@]}"; do
+                echo "Adding $i to libvirt group"
+                usermod -a -G libvirt $i
+            done
+        fi
+    fi
+fi
+#Graphics Card Driver Installer
 while true; do
     read -p "What brand is your graphics? [I]ntel, [A]MD or [N]vidia?: " graphics
     case $graphics in
     I | i)
         echo "========= Installing Intel Graphics ========="
-        pacman -S --needed --noconfirm xf86-video-intel mesa vulkan-driver vulkan-intel lib32-mesa lib32-vulkan-intel vulkan-tools i2c-tools
+        pacman -S --needed --noconfirm xf86-video-intel mesa vulkan-intel vulkan-driver lib32-mesa lib32-vulkan-intel vulkan-tools i2c-tools
         break;;
     A | a)
         echo "========= Installing AMD Graphics ========="
-        pacman -S --needed --noconfirm xf86-video-amdgpu mesa vulkan-driver vulkan-tools i2c-tools
+        pacman -S --needed --noconfirm xf86-video-amdgpu mesa
         while true; do
             read -p "Are you using a [A]MD GPU or a [R]eadon GPU? " subgpu
             case $subgpu in
             A | a)
-                pacman -S --needed --noconfirm amdvlk lib32-amdvlk
+                pacman -S --needed --noconfirm amdvlk lib32-amdvlk vulkan-driver vulkan-tools i2c-tools
                 break;;
             R | r)
-                pacman -S --needed --noconfirm vulkan-radeon lib32-vulkan-radeon
+                pacman -S --needed --noconfirm vulkan-radeon lib32-vulkan-radeon vulkan-driver vulkan-tools i2c-tools
                 break;;
             *) echo "Invaild Input";;
             esac
@@ -278,15 +312,19 @@ while true; do
         break;;
     N | n)
         echo "========= Installing Nvidia Graphics ========="
-        pacman -S --needed --noconfirm nvidia nvidia-utils nvidia-settings vulkan-driver lib32-nvidia-utils vulkan-tools i2c-tools
+        pacman -S --needed --noconfirm nvidia nvidia-utils nvidia-settings lib32-nvidia-utils vulkan-tools i2c-tools vulkan-driver
         break;;
     *) echo "Invalid input" ;;
     esac
 done
 
-
+#Font packs install
 echo "====== Installing font packs ======"
-pacman -S --needed --noconfirm dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji
+pacman -S --needed --noconfirm dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji
+
+#Base user packages
+echo "====== Installing base user packages ====="
+pacman -S --needed --noconfirm obs-studio firefox vlc papirus-icon-theme libreoffice-fresh xournalpp xclip copyq syncthing discord isync simple-scan octave htop appmenu-gtk-module emacs
 
 #DE Install
 while true; do
@@ -295,7 +333,7 @@ while true; do
     case $de in
     X | x) # XFCE
         echo "Installing XFCE and basic desktop apps"
-        pacman -S --needed --noconfirm xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4 xfce4-goodies firefox simplescreenrecorder arc-gtk-theme arc-icon-theme papirus-icon-theme vlc x11-ssh-askpass file-roller geeqie libreoffice-fresh xournalpp xclip copyq syncthing discord catfish isync xreader simple-scan gparted octave pavucontrol gtop qalculate-gtk deluge-gtk baobab nemo nemo-fileroller nemo-preview nemo-seahorse nemo-share nemo-terminal appmenu-gtk-module
+        pacman -S --needed --noconfirm xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xfce4 xfce4-goodies arc-gtk-theme arc-icon-theme file-roller geeqie catfish xreader gparted pavucontrol qalculate-gtk deluge-gtk baobab
         systemctl enable lightdm
         pacman -R --noconfirm ristretto
         if [ "$use_bluetooth" = "yes" ]; then
@@ -305,7 +343,7 @@ while true; do
         break;;
     G | g) # Gnome
         echo "Installing Gnome and basic desktop apps"
-        pacman -S --needed --noconfirm xorg gdm gnome gnome-extra firefox gnome-tweaks simplescreenrecorder arc-gtk-theme arc-icon-theme papirus-icon-theme vlc x11-ssh-askpass file-roller libreoffice-fresh syncthing discord isync simple-scan gparted octave pavucontrol gtop qalculate-gtk transmission baobab copyq
+        pacman -S --needed --noconfirm xorg gdm gnome gnome-extra gnome-tweaks arc-gtk-theme arc-icon-theme file-roller gparted pavucontrol qalculate-gtk transmission baobab
         systemctl enable gdm
         if [ "$use_bluetooth" = "yes" ]; then
             echo "Installing GUI for bluetooth"
@@ -314,7 +352,8 @@ while true; do
         break;;
     K | k) # KDE
         echo "Installing KDE and basic desktop apps"
-        pacman -S --needed --noconfirm xorg sddm plasma kde-applications firefox simplescreenrecorder papirus-icon-theme ksshaskpass libreoffice-fresh syncthing discord isync simple-scan octave pavucontrol-qt gtop qalculate-qt qbittorrent filelight copyq
+        pacman -S --needed --noconfirm xorg sddm ksshaskpass dolphin-plugins pavucontrol-qt qalculate-qt kget filelight kde-gtk-config ark audiocd-kio breeze-gtk dolphin dragon elisa gwenview kate ktorrent kde-gtk-config khotkeys kinfocenter kinit kio-fuse konsole kscreen kwallet-pam okular plasma-desktop plasma-disks plasma-nm plasma-pa powerdevil print-manager sddm-kcm solid spectacle xsettingsd kdeconnect
+
         systemctl enable sddm
         if [ "$use_bluetooth" = "yes" ]; then
             echo "Installing GUI for bluetooth"
@@ -323,7 +362,7 @@ while true; do
         break;;
     C | c) #Cinnamon
         echo "Installing Cinnamon and basic desktop apps"
-        pacman -S --needed --noconfirm xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings cinnamon firefox simplescreenrecorder arc-gtk-theme arc-icon-theme papirus-icon-theme gnome-shell x11-ssh-askpass libreoffice-fresh file-roller nemo-fileroller syncthing discord isync simple-scan gparted octave pavucontrol gtop qalculate-gtk deluge-gtk baobab copyq
+        pacman -S --needed --noconfirm xorg lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings cinnamon arc-gtk-theme arc-icon-theme gnome-shell file-roller nemo-fileroller gparted pavucontrol qalculate-gtk deluge-gtk baobab xreader
         if [ "$use_bluetooth" = "yes" ]; then
             echo "Installing GUI for bluetooth"
             pacman -S --needed --noconfirm blueman
@@ -332,6 +371,9 @@ while true; do
     *) echo "Invalid input" ;;
     esac
 done
+
+echo "Done with the graphics! Now time to move on to the bootloader"
+sleep 2
 
 #Systemd-boot or Grub Selector
 while true; do
@@ -352,7 +394,6 @@ while true; do
         echo "linux   /vmlinuz-linux" >> /boot/loader/entries/arch-latest.conf
         echo "initrd  ${microcode_hook}" >> /boot/loader/entries/arch-latest.conf
         echo "initrd  /initramfs-linux.img" >> /boot/loader/entries/arch-latest.conf
-
         root_uuid="$(findmnt -no UUID -T /)"
         root_flags="root=UUID=${root_uuid}"
         swap_flags=""
@@ -372,12 +413,33 @@ while true; do
             swap_flags="resume=UUID=${swap_uuid} resume_offset=${swap_offset}"
         fi
 
-        echo "options rw ${root_flags} ${swap_flags}" >> /boot/loader/entries/arch-latest.conf
+        if [ -z "$root_uuid" ]; then
+            echo "options rw ${root_flags} ${swap_flags}" >> /boot/loader/entries/arch-latest.conf
+        fi
         break;;
     G | g)
         echo "========= Installing GRUB ========="
-        echo "This doesn't do anything yet!";;
-    #break;;
+        pacman -S --needed --noconfirm grub efibootmgr
+        grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+        echo "Creating Boot Configurations"
+        root_uuid="$(findmnt -no UUID -T /)"
+        root_flags="root=UUID=${root_uuid}"
+        swap_flags=""
+
+        if [ "$use_crypt" = "yes" ]; then
+            root_flags="cryptdevice=[UUID of entire device where cryptroot is located]:cryptroot ${root_flags}"
+        fi
+
+        if [ "$use_swap" = "yes" ]; then
+            swap_uuid="$(findmnt -no UUID -T /swap/swapfile)"
+            swap_offset="$(sudo filefrag -v /swap/swapfile | awk '{ if($1=="0:"){print substr($4, 1, length($4)-2)} }')"
+
+            swap_flags="resume=UUID=${swap_uuid} resume_offset=${swap_offset}"
+        fi
+
+        sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT="/&'"${root_flags} ${swap_flags}"' /' /etc/default/grub
+        grub-mkconfig -o /boot/grub/grub.cfg
+        break;;
     *) echo "Invalid input" ;;
     esac
 done
