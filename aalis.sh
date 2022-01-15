6c2a09500cf528e419a589cb3bb945e309c2ab06
NovaViper <nova.code17@gmail.com>
2022-01-15 09:32:08 -0600

    3.1.0 RC4
    - Script now uses GitLab's Releases API to find online versions

--- aalis.sh
+++ aalis.sh
@@ -3,8 +3,8 @@
 set -e # Make script fail if something fails
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )" # Locate and save the script's current base directory
-VERSION=3.1.0-rc3 # The current version of the script
-ONLINE_VERSION=$(curl -s https://gitlab.com/NovaViper/aalis/-/raw/main/VERSION.txt) # URL of script's current version on the main branch
+VERSION=3.1.0-rc4 # The current version of the script
+ONLINE_VERSION=$(curl -s 'https://gitlab.com/api/v4/projects/31469197/releases' | grep tag_name | cut -d':' -f2 | cut -d'"' -f2)
 
 # Preflight check ensures that the script_funcs file (which holds all primary functions for the script)
 # is present. This file under any circumstance SHOULD NEVER be missing or really bad things will happen.
@@ -18,21 +18,7 @@ output ${LIGHT_GREEN} "Preflight Check done! Moving on in 2 seconds"
 sleep 2
 
 banner ${LIGHT_PURPLE} "Checking if script is update to date..."
-VERSION_RESULT=$(test_compare_versions $VERSION $ONLINE_VERSION || :) # use null utility from POSIX to make the script not immedately exist if the script is a beta build or outdated
-if [ "$VERSION_RESULT" == 0 ]; then
-    output ${LIGHT_GREEN} "The script is update to date!"
-elif [ "$VERSION_RESULT" == 1 ]; then
-    output ${RED} "The script is out of date; the latest version is v${ONLINE_VERSION}! The script will continue on, however it is recommended that you update the script!"
-    if [[  "yes" == $(askYesNo "Are you sure you want to continue?")  ]]; then
-       output ${YELLOW} "Ok, continuing then..."
-    fi
-elif [ "$VERSION_RESULT" == 2 ]; then
-    output ${YELLOW} "The script is ahead of the release version, so this must be a release canidate or a beta build!"
-else
-    banner ${RED} "SOMETHING HAS GONE WRONG, CANNOT DETERMINE VERSION!" "PLEASE CHECK INTERNET CONNECTION OR REPORT THIS ERROR TO THE PROJECT'S GITLAB"
-    sleep 2
-    exit
-fi
+test_compare_versions $VERSION $ONLINE_VERSION || :
 
 banner ${LIGHT_PURPLE} "Setting font size"
 installPac "terminus-font figlet"
