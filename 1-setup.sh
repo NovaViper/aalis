7f32df8d8fa2c0ecb14b941e0f96a6d83abf6d4d
NovaViper <nova.code17@gmail.com>
2022-04-07 16:01:56 -0500

    Version 4.1.2
    - Added more environmental variables for XDG compliance

--- 1-setup.sh
+++ 1-setup.sh
@@ -135,6 +135,7 @@ xdg-user-dirs-update # Updates user directories for XDG Specification
 output ${YELLOW} "Configuring environment variables for XDG specification"
 cat <<-"EOF" >> /etc/profile
 
+export GPG_TTY="$(tty)"
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_HOME="$HOME/.local/share"
@@ -144,9 +145,17 @@ export CARGO_HOME="$XDG_DATA_HOME/cargo"
 export LESSHISTFILE="$XDG_CONFIG_HOME/less/history"
 export LESSKEY="$XDG_CONFIG_HOME/less/keys"
 export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm"
-export BASH_COMPLETION_USER_FILE="$XDG_CONFIG_HOME"/bash-completion/bash_completion
-export GTK_RC_FILES="$XDG_CONFIG_HOME"/gtk-1.0/gtkrc
-export GTK2_RC_FILES="$XDG_CONFIG_HOME"/gtk-2.0/gtkrc
+export BASH_COMPLETION_USER_FILE="$XDG_CONFIG_HOME/bash-completion/bash_completion"
+export GTK_RC_FILES="$XDG_CONFIG_HOME/gtk-1.0/gtkrc"
+export GTK2_RC_FILES="$XDG_CONFIG_HOME/gtk-2.0/gtkrc"
+export NUGET_PACKAGES="$XDG_CACHE_HOME/NuGetPackages"
+export _JAVA_OPTIONS=-Djava.util.prefs.userRoot="$XDG_CONFIG_HOME/java"
+export OCTAVE_HISTFILE="$XDG_CACHE_HOME/octave-hsts"
+export OCTAVE_SITE_INITFILE="$XDG_CONFIG_HOME/octave/octaverc"
+export XINITRC="$XDG_CONFIG_HOME/X11/xinitrc"
+export KDEHOME="$XDG_CONFIG_HOME/kde"
+export ICEAUTHORITY="$XDG_CACHE_HOME/ICEauthority"
+export PASSWORD_STORE_DIR="$XDG_CONFIG/pass"
 EOF
 
 if [[ "yes" == $(askYesNo "Would you like to use the dotfiles manager, YADM?") ]]; then
